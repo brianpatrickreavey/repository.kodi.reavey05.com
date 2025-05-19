accce9f551d5241055668b93260bd6a951fe3641
Brian Patrick Reavey <brianpatrickreavey@users.noreply.github.com>
2025-05-19 06:52:16 -0400

    Scaffolding merge 1 (#1)
    * Initial scaffolding for the Repository
    * Add angelstudios addon to the repository
    * add installation of `gitpython` to the build environment
    * generate an index.html page for the site
    * fix cname getting overwritten
    * cleanup  and prep for merge
    * updating to new schema
    * Convert hashing to sha256, use local copy of the `create_repository.py` script
    * Update workflow to allow manual branch selection and workflow kickoff, and bump version to 0.1.3
    * cleanup and prep for merge to `main` to enable `workflow_dispatch`

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # respository.kodi.reavey05.com
 KODI Addons
+
+# Current status
+Building out the framework to publish to GitHub Pages under repository.kodi.reavey05.com
+
+* repository.kodi.reavey05.com CNAME to brianpatrickreavey.github.io
+* Built using the directions here: https://kodi.wiki/view/Add-on_repositories#Repository_Tools
---
+++ tools/generate_index.py
@@ -0,0 +1,39 @@
+import os
+
+html = '''
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>KODI Addons @ repository.kodi.reavey05.com</title>
+</head>
+<body>
+  <h1>KODI Addons @ repository.kodi.reavey05.com</h1>'''
+
+
+# if gh-pages exists
+if not os.path.exists('gh-pages'):
+    exit(1)
+# for each directory in the gh-pages
+for dir in sorted(os.listdir('gh-pages')):
+    dir_path = os.path.join('gh-pages', dir)
+    print(f"{dir_path=}")
+    if os.path.isdir(dir_path) and not dir.startswith('.'):
+        html += f'  <h2>{dir}</h2>\n  <ul>\n'
+        for file in sorted(os.listdir(dir_path)):
+            file_path = os.path.join(dir_path, file)
+            print(f"{file_path=}")
+            if file.endswith('.zip'):
+                print(f"Found zip file: {file_path}")
+                # Relative path from 'gh-pages' directory
+                rel_path = f"{dir}/{file}"
+                html += f'    <li><a href="https://repository.kodi.reavey05.com/{rel_path}">{file}</a></li>\n'
+        html += '   </ul>\n'
+
+html += '''
+</body>
+</html>'''
+
+# write the HTML to a file
+with open('gh-pages/index.html', 'w') as f:
+    f.write(html)
